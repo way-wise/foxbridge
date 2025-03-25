7e828826dc6991b75f23f387b9e70358f6202049
waywise <waywisetechdevelopers@gmail.com>
2025-03-25 13:11:55 +0600

    style: header menu option updated

--- include/header.php
+++ include/header.php
@@ -1,158 +1,221 @@
- 
 <header class="site-navbar  site-navbar-target">
-	<div class="container">
-		<div class="row align-items-center mobileheader">
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 col-4 d-sm-block d-lg-none d-xl-none d-xxl-none">
-				<div class="cavasoff d-flex align-items-center">
-					<div class="canvas_btn">
-						<div></div>
-						<div></div>
-						<div></div>
-					</div>
-					<span>Menu</span>
-				</div>
-			</div>
-			<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 col-4 ">
-				<div class="logo text-center">
-					<a href="index.php">
-						<img src="images/logo.png">
-					</a>
-				</div>
-			</div>
-            <div class="col-lg-10 col-md-4 col-sm-4 col-xs-4 col-4 d-sm-none d-lg-block d-xl-block d-xxl-block">
-            
+   <div class="container">
+      <div class="row align-items-center mobileheader">
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 col-4 d-sm-block d-lg-none d-xl-none d-xxl-none">
+            <div class="cavasoff d-flex align-items-center">
+               <div class="canvas_btn">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+               </div>
+               <span>Menu</span>
+            </div>
+         </div>
+         <div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 col-4 ">
+            <div class="logo text-center">
+               <a href="index.php">
+                  <img src="images/logo.png">
+               </a>
+            </div>
+         </div>
+         <div class="col-lg-10 col-md-4 col-sm-4 col-xs-4 col-4 d-sm-none d-lg-block d-xl-block d-xxl-block">
+
+            <!-- <nav class="site-navigation position-relative text-right" role="navigation">
+               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Golf</b></a>
+                     <ul class="dropdown">
+                        <li><a href="https://www.foxbridge.ca/tee-time" target="_blank" class="nav-link">Book Tee
+                              Time</a></li>
+                        <li><a href="courses.php" class="nav-link">Courses</a></li>
+                        <li><a href="tournament.php" class="nav-link">Tournament</a></li>
+                        <li><a href="League.php" class="nav-link">Leagues</a></li>
+                        <li><a href="membership.php" class="nav-link">Memberships</a></li>
+                        <li><a href="camps.php" class="nav-link">Summer Camp</a></li>
+                        <li><a href="Corporategolf.php" class="nav-link">Corporate Golf</a></li>
+                        <li><a href="https://www.foxbridge.ca/tee-time/simulator" class="nav-link">Indoor Simulator</a>
+                        </li>
+                        <li><a href="policy.php" class="nav-link">Policies</a></li>
+                     </ul>
+                  </li>
+
+
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Events</b></a>
+                     <ul class="dropdown">
+                        <li> <a href="venues.php" class="nav-link">Event space </a></li>
+
+                        <li> <a href="events.php" class="nav-link">Weddings & Events</a></li>
+                        <li> <a href="gallery.php" class="nav-link">Gallery </a></li>
+                        <li> <a href="packages.php" class="nav-link">Packages</a></li>
+                        <li> <a href="upcoming-events.php" class="nav-link">Upcoming Events</a></li>
+                     </ul>
+                  </li>
+
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Restaurant</b></a>
+                     <ul class="dropdown">
+                        <li><a href="dining-bar.php" class="nav-link">Dining & Bar</a> </li>
+                     </ul>
+                  </li>
+
+
+
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>More</b></a>
+                     <ul class="dropdown">
+                        <li> <a href="about-us.php" class="nav-link">About Foxbridge</a></li>
+                        <li><a href="contacts.php" class="nav-link">Contact</a>
+                        <li> <a href="jobs.php" class="nav-link">Employment</a></li>
+                        <li> <a href="history.php" class="nav-link">Our History</a></li>
+                        <li> <a href="location.php" class="nav-link">Our Location </a></li>
+
+                     </ul>
+                  </li>
+
+
+            </nav> -->
             <nav class="site-navigation position-relative text-right" role="navigation">
-              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-        <!-- <li><a href="index.php" class="nav-link">Home </a> </li>-->
-    <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Golf</b></a> 
-      <ul class="dropdown">
-      	 <li><a href="https://www.foxbridge.ca/tee-time" target="_blank"  class="nav-link">Book Tee Time</a></li>
-         <li><a href="courses.php" class="nav-link">Courses</a></li>
-          <li><a href="tournament.php" class="nav-link">Tournament</a></li>
-         <li><a href="League.php" class="nav-link">Leagues</a></li>
-         <li><a href="membership.php" class="nav-link">Memberships</a></li>
-         <li><a href="camps.php" class="nav-link">Summer Camp</a></li>
-         <li><a href="Corporategolf.php" class="nav-link">Corporate Golf</a></li>
-         <li><a href="https://www.foxbridge.ca/tee-time/simulator" class="nav-link">Indoor Simulator</a> </li> 
-        <!-- <li><a href="promotions.php" class="nav-link">Promotions</a></li>-->
-         <li><a href="policy.php" class="nav-link">Policies</a></li>
-      </ul>
-   </li>
+               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                  <!-- <li><a href="index.php" class="nav-link">Home </a> </li>-->
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Golf</b></a>
+                     <ul class="dropdown">
+                        <li><a href="https://www.foxbridge.ca/tee-time" target="_blank" class="nav-link">Book Tee
+                              Time</a></li>
+                        <li><a href="tournament.php" class="nav-link">Tournaments</a></li>
+                        <li><a href="League.php" class="nav-link">Leagues</a></li>
+                        <li><a href="membership.php" class="nav-link">Memberships</a></li>
+                        <li><a href="camps.php" class="nav-link">Summer Camp</a></li>
+                        <li><a href="Corporategolf.php" class="nav-link">Corporate Golf</a></li>
+                        <li><a href="https://www.foxbridge.ca/tee-time/simulator" class="nav-link">Indoor Simulator</a>
+                        </li>
+                        <li><a href="courses.php" class="nav-link">Our Courses</a></li>
+                        <!-- <li><a href="promotions.php" class="nav-link">Promotions</a></li>-->
+                        <li><a href="policy.php" class="nav-link">Policies</a></li>
+                     </ul>
+                  </li>
 
 
-   <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Events</b></a> 
-      <ul class="dropdown">
-         <li> <a href="venues.php" class="nav-link">Event space </a></li> 
-          
-           <li> <a href="events.php" class="nav-link">Weddings & Events</a></li>        
-         <!--<li> <a href="vendors.php" class="nav-link">Vendor list </a></li> -->
-         <li> <a href="gallery.php" class="nav-link">Gallery </a></li> 
-         <li> <a href="packages.php" class="nav-link">Packages</a></li>
-         <li> <a href="upcoming-events.php" class="nav-link">Upcoming Events</a></li>
-      </ul>
-   </li> 
-   <!--<li><a href="https://www.destinaholidays.com/" target="_blank" class="nav-link">Flights</a> </li> -->
-   
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Events</b></a>
+                     <ul class="dropdown">
+                        <li> <a href="venues.php" class="nav-link">Event space</a></li>
+                        <li> <a href="events.php" class="nav-link">Social Events</a></li>
+                        <li> <a href="gallery.php" class="nav-link">Events Gallery</a></li>
+                        <li> <a href="packages.php" class="nav-link">Event Packages</a></li>
+                        <li> <a href="upcoming-events.php" class="nav-link">Upcoming Events</a></li>
+                        <li> <a href="contact-for-events.php" class="nav-link">Contact us</a></li>
+                     </ul>
+                  </li>
+                  <!--<li><a href="https://www.destinaholidays.com/" target="_blank" class="nav-link">Flights</a> </li> -->
 
-    <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Restaurant</b></a> 
-      <ul class="dropdown">
-   <li><a href="dining-bar.php" class="nav-link">Dining & Bar</a> </li>
-   </ul>
-   </li>
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Weddings</b></a>
+                     <ul class="dropdown">
+                        <li> <a href="venues.php" class="nav-link">Event space</a></li>
 
-  
-
-   <li class="has-children"><a href="javascript:void();" class="nav-link"><b>More</b></a> 
-      <ul class="dropdown">
-        <li> <a href="about-us.php" class="nav-link">About Foxbridge</a></li>
-        <li><a href="contacts.php" class="nav-link">Contact</a> 
-        <li> <a href="jobs.php" class="nav-link">Employment</a></li> 
-         <li> <a href="history.php" class="nav-link">Our History</a></li>
-         <li> <a href="location.php" class="nav-link">Our Location </a></li>
-         
-      </ul>
-   </li>
+                        <!--<li> <a href="vendors.php" class="nav-link">Vendor list </a></li> -->
+                        <li> <a href="gallery.php" class="nav-link">Wedding Gallery </a></li>
+                        <li> <a href="packages.php" class="nav-link">Wedding Packages</a></li>
+                        <li> <a href="book-a-tour.php" class="nav-link">Book a Tour</a></li>
+                     </ul>
+                  </li>
 
 
+
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>Scrambles Restaurant</b></a>
+                     <ul class="dropdown">
+                        <li><a href="dining-bar.php" class="nav-link">Dining &amp; Bar</a> </li>
+                     </ul>
+                  </li>
+
+
+
+                  <li class="has-children"><a href="javascript:void();" class="nav-link"><b>More</b></a>
+                     <ul class="dropdown">
+                        <li> <a href="about-us.php" class="nav-link">About Foxbridge</a></li>
+                        <li><a href="contacts.php" class="nav-link">Contact</a>
+                        </li>
+                        <li> <a href="jobs.php" class="nav-link">Employment</a></li>
+                        <li> <a href="history.php" class="nav-link">Our History</a></li>
+                        <li> <a href="location.php" class="nav-link">Our Location </a></li>
+
+                     </ul>
+                  </li>
+
+
+               </ul>
             </nav>
-            </div>            
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 col-4 hideonlgSreen">
-				<ul class="left_menu float-end">
-				    <li><a href="javascript:void();" class="d-lg-block" data-bs-toggle="offcanvas" data-bs-target="#BookTeaTime">Book Now</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-    
-    
-	<div class="cavasoff_menu">
-		<div class="close_btn float-end">&#x2715;</div>
-	
-        
-      <!--nav-->  
-    <ul class="menu">
-      <!--<li><a href="index.php">Home </a> </li>-->
-    <li class="list"><a href="javascript:void();">Golf </a> 
-      <ul class="items">
-      	 <li><a href="https://www.foxbridge.ca/tee-time" target="_blank">Book Tee Time</a></li>
-         <li><a href="courses.php">Courses</a></li>
-          <li><a href="tournament.php">Tournament</a></li>
-         <li><a href="League.php" class="nav-link">Leagues</a></li>
-         <li><a href="membership.php" class="nav-link">Memberships</a></li>
-         <li><a href="camps.php" class="nav-link">Academy</a></li>
-         <li><a href="Corporategolf.php" class="nav-link">Corporate Golf</a></li>
-         <li><a href="https://www.foxbridge.ca/tee-time/simulator" class="nav-link">Indoor Simulator</a> </li> 
-         <li><a href="policy.php" class="nav-link">Policies</a></li>
-      </ul>
-   </li>
+         </div>
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 col-4 hideonlgSreen">
+            <ul class="left_menu float-end">
+               <li><a href="javascript:void();" class="d-lg-block" data-bs-toggle="offcanvas"
+                     data-bs-target="#BookTeaTime">Book Now</a></li>
+            </ul>
+         </div>
+      </div>
+   </div>
 
-     <li class="list"><a href="javascript:void();">Events</a> 
-      <ul class="items">
-          <li> <a href="venues.php">Event space </a></li> 
-          <li> <a href="events.php">Weddings & Events</a></li>   
-          <li> <a href="gallery.php" class="nav-link">Gallery </a></li>
-          <li> <a href="packages.php" class="nav-link">Packages</a></li>
-          <li> <a href="upcoming-events.php" class="nav-link">Upcoming Events</a></li>
-      </ul>
-   </li> 
 
-    <li class="list"><a href="javascript:void();"><b>Restaurant</a> 
-      <ul class="items">
-      <li><a href="dining-bar.php">Dining & Bar</a> </li>
-      </ul>
-   </li>
+   <div class="cavasoff_menu">
+      <div class="close_btn float-end">&#x2715;</div>
 
- <li class="list"><a href="javascript:void();">More></a> 
-      <ul class="items">
-        <li> <a href="about-us.php">About Foxbridge</a></li>
-        <li><a href="contacts.php" class="nav-link">Contact us</a> 
-        <li> <a href="jobs.php" class="nav-link">Employment</a></li>
-        <li> <a href="history.php">Our History</a></li>
-        <li> <a href="location.php">Our Location </a></li>
-         <!--<li> <a href="jobs.php">Jobs </a></li>-->
-      </ul>
-   </li>
 
-   
+      <!--nav-->
+      <ul class="menu">
+         <!--<li><a href="index.php">Home </a> </li>-->
+         <li class="list"><a href="javascript:void();">Golf </a>
+            <ul class="items">
+               <li><a href="https://www.foxbridge.ca/tee-time" target="_blank">Book Tee Time</a></li>
+               <li><a href="courses.php">Courses</a></li>
+               <li><a href="tournament.php">Tournament</a></li>
+               <li><a href="League.php" class="nav-link">Leagues</a></li>
+               <li><a href="membership.php" class="nav-link">Memberships</a></li>
+               <li><a href="camps.php" class="nav-link">Academy</a></li>
+               <li><a href="Corporategolf.php" class="nav-link">Corporate Golf</a></li>
+               <li><a href="https://www.foxbridge.ca/tee-time/simulator" class="nav-link">Indoor Simulator</a> </li>
+               <li><a href="policy.php" class="nav-link">Policies</a></li>
+            </ul>
+         </li>
 
-            </nav>
-            </div>  
-	 <!--nav-->    
-        
-        
-	</div>
-	<div class="paypopup search tc">
-	    <div class="poppup_box">
-	        <div class="pop_close closer">x</div>
-	        <div class="content">
-	            <form action="" method="get">
-	            	<input type="text" name="" placeholder="Search...">
-	            	<button><i class="fa fa-search"></i></button>
-	            </form>
-	        </div>
-	    </div>
-	</div>
+         <li class="list"><a href="javascript:void();">Events</a>
+            <ul class="items">
+               <li> <a href="venues.php">Event space </a></li>
+               <li> <a href="events.php">Weddings & Events</a></li>
+               <li> <a href="gallery.php" class="nav-link">Gallery </a></li>
+               <li> <a href="packages.php" class="nav-link">Packages</a></li>
+               <li> <a href="upcoming-events.php" class="nav-link">Upcoming Events</a></li>
+            </ul>
+         </li>
+
+         <li class="list"><a href="javascript:void();"><b>Restaurant</a>
+            <ul class="items">
+               <li><a href="dining-bar.php">Dining & Bar</a> </li>
+            </ul>
+         </li>
+
+         <li class="list"><a href="javascript:void();">More></a>
+            <ul class="items">
+               <li> <a href="about-us.php">About Foxbridge</a></li>
+               <li><a href="contacts.php" class="nav-link">Contact us</a>
+               <li> <a href="jobs.php" class="nav-link">Employment</a></li>
+               <li> <a href="history.php">Our History</a></li>
+               <li> <a href="location.php">Our Location </a></li>
+               <!--<li> <a href="jobs.php">Jobs </a></li>-->
+            </ul>
+         </li>
+
+
+
+         </nav>
+   </div>
+   <!--nav-->
+
+
+   </div>
+   <div class="paypopup search tc">
+      <div class="poppup_box">
+         <div class="pop_close closer">x</div>
+         <div class="content">
+            <form action="" method="get">
+               <input type="text" name="" placeholder="Search...">
+               <button><i class="fa fa-search"></i></button>
+            </form>
+         </div>
+      </div>
+   </div>
 </header>
-
-
- 
